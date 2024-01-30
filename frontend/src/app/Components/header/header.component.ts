7751717d6e871d89d71e7ae0a3eed25d01f6b7cf
Eric-Philippe <ericphlpp@gmail.com>
2024-01-30 18:28:02 +0100

    Welcome Page

--- frontend/src/app/Components/header/header.component.ts
+++ frontend/src/app/Components/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  showPhoneMenu = true;
+  showPhoneMenu = false;
 }
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -20,12 +20,38 @@ export class AppComponent {
   title = 'frontend';
   randomBook = RANDOM_BOOKS[Math.floor(Math.random() * RANDOM_BOOKS.length)];
   displayBanner = true;
+  isLibrairyOpen = false;
 
   session = Session.getInstance();
 
   constructor() {
     this.session.login('member@member.com', 'admin');
-    console.log(this.session.user);
+    // La bibliothèque est ouverte du mardi au vendredi de 10h à 12h / 14h à 18h et le samedi de 10h à 12h / 14h à 17h
+    const date = new Date();
+    const day = date.getDay();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    if (day > 1 && day < 6) {
+      if (hour >= 10 && hour < 12) {
+        this.isLibrairyOpen = true;
+      } else if (hour >= 14 && hour < 18) {
+        this.isLibrairyOpen = true;
+      } else if (hour === 12 && minute === 0) {
+        this.isLibrairyOpen = true;
+      } else if (hour === 18 && minute === 0) {
+        this.isLibrairyOpen = true;
+      }
+    } else if (day === 6) {
+      if (hour >= 10 && hour < 12) {
+        this.isLibrairyOpen = true;
+      } else if (hour >= 14 && hour < 17) {
+        this.isLibrairyOpen = true;
+      } else if (hour === 12 && minute === 0) {
+        this.isLibrairyOpen = true;
+      } else if (hour === 17 && minute === 0) {
+        this.isLibrairyOpen = true;
+      }
+    }
   }
 
   hideBanner() {
