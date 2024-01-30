196c7a6dbecddac215befeb9edba7e338b9631e3
Eric-Philippe <ericphlpp@gmail.com>
2024-01-30 02:19:11 +0100

    Downgrade Angular to 16.2

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+const RANDOM_BOOKS = [
+  'Le Seigneur des Anneaux',
+  'La dernière Erdane',
+  'La guerre des mondes',
+  'Le meilleur des mondes',
+];
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
+  randomBook = RANDOM_BOOKS[Math.floor(Math.random() * RANDOM_BOOKS.length)];
 }
