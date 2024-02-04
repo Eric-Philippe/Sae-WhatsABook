6e97d08df0ef4ea79253bc7689d8b618739b560d
Eric-Philippe <ericphlpp@gmail.com>
2024-02-04 19:24:01 +0100

    Add Swagger

--- server/config/bundles.php
+++ server/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
